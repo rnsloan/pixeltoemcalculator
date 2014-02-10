f356c4cc3129f60c75b12c41446da3205625c1a8
rnsloan <rnsloan1@gmail.com>
2014-02-10 18:52:22 +1100

    Added event listener for converting ems to pixels Added tests for DOM events

--- src/js/pxtoem.js
+++ src/js/pxtoem.js
@@ -10,19 +10,29 @@ PxToEm.prototype.convertToPixels = function (emsValue) {
     return (emsValue * this.basePixelValue).toFixed(4);
 }
 
-PxToEm.prototype.updateEmValue = function () {
-    var that = this;
-
-    var baseInput = document.getElementById('base');
+PxToEm.prototype.updateEmValue = function (context) {
     var pixelInput = document.getElementById('pixel');
     var emInput = document.getElementById('em');
 
-    pixelInput.addEventListener('keyup', function () {
+    //simulating/testing native keyup events is difficult
+    $('#pixel').on('keyup', function() {
         var pixelValue = pixelInput.value;
-        emInput.value = that.convertToEms(pixelValue);
-    }, false);
+        emInput.value = context.convertToEms(pixelValue);
+    });
+}
+
+PxToEm.prototype.updatEmValue = function (context) {
+    var pixelInput = document.getElementById('pixel');
+    var emInput = document.getElementById('em');
+
+    //simulating/testing native keyup events is difficult
+    $('#em').on('keyup', function() {
+        var emValue = emInput.value;
+        pixelInput.value = context.convertToPixels(emValue);
+    });
 }
 
 PxToEm.prototype.init = function () {
-    this.updateEmValue();
+    this.updateEmValue(this);
+    this.updatEmValue(this);
 }
--- tests/tests.js
+++ tests/tests.js
@@ -2,29 +2,52 @@ module("Conversion methods", {
     setup: function () {
         testPxToEm = new PxToEm();
     },
-    teardown: function() {
+    teardown: function () {
         testPxToEm = {};
     }
 });
-test( "conversion methods", function() {
+test("conversion methods", function () {
     equal(testPxToEm.convertToEms(16), 1, 'convert from pixels to ems')
     equal(testPxToEm.convertToEms(33), 2.0625, 'convert from pixels to ems')
 
     equal(testPxToEm.convertToPixels(1), 16, 'convert from ems to pixels')
 })
 
-module("Conversion methods  with different base font size", {
+module("Conversion methods with different base font size", {
     setup: function () {
         changeBaseSize = new PxToEm();
         changeBaseSize.basePixelValue = 23;
     },
-    teardown: function() {
+    teardown: function () {
         changeBaseSize = {};
     }
 });
-test( "conversion methods with different base size", function() {
+test("conversion methods with different base size", function () {
     equal(changeBaseSize.convertToEms(16), 0.6957, 'check output is rounded to 4 decimal places')
     equal(changeBaseSize.convertToPixels(2.3456), 53.9488, 'convert from ems to pixels')
 });
 
-//TODO: functional tests
+module("DOM events", {
+    setup: function () {
+        $('<input id="base" type="number" required value="16" /> \
+            <input id="pixel" type="number" /> \
+            <input id="em" type="number" />').appendTo('#qunit-fixture');
+
+        testPxToEm = new PxToEm();
+        testPxToEm.init();
+    },
+    teardown: function () {
+        testPxToEm = {};
+    }
+});
+test("pixel input dom event", function () {
+    $('#pixel').val('10').trigger('keyup');
+
+    equal( $('#em').val() , '0.6250', 'em to pixel conversion')
+});
+
+test("pixel input dom event", function () {
+    $('#em').val('2').trigger('keyup');
+
+    equal( $('#pixel').val() , '32.0000', 'pixel to em conversion')
+});
